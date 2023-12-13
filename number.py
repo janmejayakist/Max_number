b37988a52ba9211bfbdfcb1fd2848b62fce4582d
janmejayakist <153728750+janmejayakist@users.noreply.github.com>
2023-12-13 16:36:26 +0530

    number.py

---
+++ number.py
@@ -0,0 +1,19 @@
+# Max_number
+import streamlit as st
+
+def find_largest_number(num1, num2, num3):
+    return max(num1, num2, num3)
+
+def main():
+    st.title("Largest Number Finder")
+
+    num1 = st.number_input("Enter the first number:")
+    num2 = st.number_input("Enter the second number:")
+    num3 = st.number_input("Enter the third number:")
+
+    if st.button("Find Largest Number"):
+        largest_number = find_largest_number(num1, num2, num3)
+        st.success(f"The largest number is: {largest_number}")
+
+if _name_ == "_main_":
+    main()
